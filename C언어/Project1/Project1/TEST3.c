4de9a3899460daa4099187901a517b6397661e65
2DAM96 <xotjqwh@gmail.com>
2026-03-17 17:03:00 +0900

    수업내용
    반영

---
+++ C언어/Project1/Project1/TEST3.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	const int c = 1;
+	//ci = 1; 값이 변하였다. 그래서 동일 선상에서 변수 초기값 설정
+}
+
+/*
+타입 별칭 정의 typedef 
+타입 정의해서 쓸게  선언
+	ex ) typedef const, unsigned long long CULL;
+
+
+*/
---
+++ C언어/Project1/Project1/TEST4.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	const int c = 1;
+	//ci = 1; 값이 변하였다. 그래서 동일 선상에서 변수 초기값 설정
+
+
+	printf("홍길동");
+}
+
+/*
+한글쓰기
+
+
+제어판 국가또는 지역 > 관리자 옵션 > 체크박스 체크 > 확인하면 한글 출력 가능함 안깨짐
+
+
+*/
