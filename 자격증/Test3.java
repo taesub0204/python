6d5711da69e0122e7f42dfd48f8a607e4af65fd4
taesub0204 <xotjqwh@gmail.com>
2026-05-14 00:48:26 +0900

    반영
    반영

---
+++ 자격증/Test3.java
@@ -0,0 +1,11 @@
+public class Test3 {
+    public static void main(String [] args)
+    {
+        int j = 024;
+        int k = 24; 
+        int l = 0x24;
+        int hap = j + k + l ;
+        System.out.printf("%d, %d, %d , %d", j,k,l,hap);
+    }
+    
+}
