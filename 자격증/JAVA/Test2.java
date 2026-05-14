2857be10746d37cf223108de3ad1cf1b11c6a0b3
taesub0204 <xotjqwh@gmail.com>
2026-05-14 18:36:09 +0900

    자격증 연습 반영
    자격증 연습 반영

---
+++ 자격증/JAVA/Test2.java
@@ -0,0 +1,17 @@
+// 10진수를 2진수로
+
+public class Test2{
+    public static void main(String[] args){
+        int a[] = new int[8];
+        int i = 0;
+        int n = 10;
+        while(i < a.length){
+            a[i++] = n % 2;
+            n/= 2;
+        }
+        for (i = 7; i >= 0; i--){
+            System.out.print( a[i]);
+        }
+
+    }
+}
